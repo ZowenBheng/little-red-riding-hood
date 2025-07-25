6ec20be520a7b10347477141e7f1764205eca8fd
ZowenBheng <brilliantbowen@icloud.com>
2025-07-25 12:28:58 +1200

    respawns

--- scripts/Spiky boi.gd
+++ scripts/Spiky boi.gd
@@ -3,4 +3,4 @@ extends Area2D
 func _on_body_entered(body: CharacterBody2D) -> void:
 	
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://UI/respawn_button.tscn")
+		get_tree().change_scene_to_file("res://UI/respawn_unexpected.tscn")
