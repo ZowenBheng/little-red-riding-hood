6e5d17d8026a0d2e0da020e9725951b13617dfcf
ZowenBheng <brilliantbowen@icloud.com>
2025-10-15 14:10:09 +1300

    ethern tried to delete this

--- levels/endingrd.gd
+++ levels/endingrd.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		print("detected")
-		#get_tree().change_scene_to_file("res://levels/ending.tscn")
+		get_tree().change_scene_to_file("res://levels/ending.tscn")
 		
